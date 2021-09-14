df798c6305290dfb00fd5d4f0d64ef9a140a77f2
mattegan111 <mnegan93@gmail.com>
2021-09-14 20:35:14 +1200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const hamburgerMenu = document.getElementById('hamburger-menu');
+const mobileMenuContainer = document.getElementById('mobile-menu-container');
+
+hamburgerMenu.addEventListener('click', () => {
+  if(mobileMenuContainer.classList.contains('make-absolute')){
+    mobileMenuContainer.classList.remove('make-absolute');
+  } else {
+    mobileMenuContainer.classList.add('make-absolute');
+  }
+  console.log('click!')
+})
+
