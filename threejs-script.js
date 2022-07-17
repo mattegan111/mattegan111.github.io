5b50f42fa96f9faa336f0dfa4913afb5e75c2491
Matthew Egan <mnegan93@gmail.com>
2022-07-18 00:08:46 +1200

    Create seperate js script for threejs

---
+++ threejs-script.js
@@ -0,0 +1,28 @@
+import * as THREE from '/build/three.module.js';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+const geometryIcosahedron = new THREE.IcosahedronGeometry(5, 0);
+const materialIcosahedron = new THREE.MeshStandardMaterial({ color: 0xb16cea });
+const meshIcosahedron = new THREE.Mesh(geometryIcosahedron, materialIcosahedron);
+
+scene.add(meshIcosahedron);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(20,20,20);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.125);
+
+scene.add(pointLight, ambientLight);
