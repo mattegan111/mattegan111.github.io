b7716eb23beb38a1fe2fe633a065e8ec8546d716
Matthew Egan <mnegan93@gmail.com>
2022-07-19 08:12:52 +1200

    Adjust box color and lighting intensity

--- script.js
+++ script.js
@@ -15,31 +15,43 @@ import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 const scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xffffff );
 
-const camera = new THREE.PerspectiveCamera( 45, 360 / 360, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera( 45, 320 / 400, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(360, 360);
+renderer.setSize(320, 400);
 camera.position.setZ(13);
 
 renderer.render(scene, camera);
 
-const geometryBox = new THREE.BoxGeometry(5, 5, 5);
-const materialBox = new THREE.MeshStandardMaterial({ color: 0xB27BE5 });
-const meshBox = new THREE.Mesh(geometryBox, materialBox);
-if(window.innerWidth > 400){
-  meshBox.position.setX(-0.5);
-}
+const geometrySphere = new THREE.SphereGeometry(6, 64, 64);
+const materialSphere = new THREE.MeshBasicMaterial({ color: 0xFF5E69 });
+const meshSphere = new THREE.Mesh(geometrySphere, materialSphere);
+meshSphere.position.set(-.325, .25, -7)
 
-scene.add(meshBox);
+const geometryBox = new THREE.BoxGeometry(1, 1, 1);
+const materialBoxOne = new THREE.MeshStandardMaterial({ color: 0xFF785E });
+const meshBoxOne = new THREE.Mesh(geometryBox, materialBoxOne);
+meshBoxOne.position.set(-1.5, 4, 0);
+
+const materialBoxTwo = new THREE.MeshStandardMaterial({ color: 0xB06BE9 });
+const meshBoxTwo = new THREE.Mesh(geometryBox, materialBoxTwo);
+meshBoxTwo.position.set(-3, 0.5, 0);
+
+const materialBoxThree = new THREE.MeshStandardMaterial({ color: 0xFFA84B });
+const meshBoxThree = new THREE.Mesh(geometryBox, materialBoxThree);
+meshBoxThree.position.set(3, 1.5, 0);
+
+
+scene.add(meshSphere, meshBoxOne, meshBoxTwo, meshBoxThree);
 
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(20,20,20);
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.25);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 
 scene.add(pointLight, ambientLight);
 
@@ -52,9 +64,15 @@ scene.add(pointLight, ambientLight);
 function animate() {
   requestAnimationFrame(animate);
 
-  meshBox.rotation.x += 0.0018;
-  meshBox.rotation.y += 0.001;
-  meshBox.rotation.z += 0.0018;
+  meshBoxOne.rotation.x += 0.0018;
+  meshBoxOne.rotation.y += 0.001;
+  meshBoxOne.rotation.z += 0.0018;
+  meshBoxTwo.rotation.x += 0.0018;
+  meshBoxTwo.rotation.y += 0.001;
+  meshBoxTwo.rotation.z += 0.0018;
+  meshBoxThree.rotation.x += 0.0018;
+  meshBoxThree.rotation.y += 0.001;
+  meshBoxThree.rotation.z += 0.0018;
 
   renderer.render(scene, camera);
 }
