091dc026e074b38049e03b72e992976b1dc5bee1
Matthew Egan <mnegan93@gmail.com>
2022-07-18 16:07:42 +1200

    Make Box positioning not apply to mobile sized screens

--- script.js
+++ script.js
@@ -30,8 +30,9 @@ renderer.render(scene, camera);
 const geometryBox = new THREE.BoxGeometry(5, 5, 5);
 const materialBox = new THREE.MeshStandardMaterial({ color: 0xb16cea });
 const meshBox = new THREE.Mesh(geometryBox, materialBox);
-meshBox.position.setX(-0.5);
-
+if(window.innerWidth > 350){
+  meshBox.position.setX(-0.5);
+}
 
 scene.add(meshBox);
 
